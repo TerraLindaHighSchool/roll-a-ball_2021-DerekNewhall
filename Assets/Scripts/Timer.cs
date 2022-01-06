618de593f0271fe9de3e51f6753612589b609927
DerekNewhall <89489562+DerekNewhall@users.noreply.github.com>
2022-01-06 15:38:40 -0800

    Game Complete

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class LevelTimer : MonoBehaviour
+{
+    TextMeshProUGUI timerText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timerText = GetComponentInChildren<TMPro.TextMeshProUGUI>(); // cache the text component
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float t = Time.timeSinceLevelLoad; // time since scene loaded
+
+        float milliseconds = (Mathf.Floor(t * 100) % 100); // calculate the milliseconds for the timer
+
+        int seconds = (int)(t % 60); // return the remainder of the seconds divide by 60 as an int
+        t /= 60; // divide current time y 60 to get minutes
+        int minutes = (int)(t % 60); //return the remainder of the minutes divide by 60 as an int
+        t /= 60; // divide by 60 to get hours
+        int hours = (int)(t % 24); // return the remainder of the hours divided by 60 as an int
+
+        timerText.text = string.Format("{0}:{1}:{2}.{3}", hours.ToString("00"), minutes.ToString("00"), seconds.ToString("00"), milliseconds.ToString("00"));
+    }
+}
